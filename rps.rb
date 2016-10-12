62e131cd354b32d977626376a3f3d7b3a2e54284
Yaniv Kleinman <yaniv.kleinman@gmail.com>
2016-10-11 19:54:50 -0500

    game_version 1

--- rps.rb
+++ rps.rb
@@ -1,1 +1,25 @@
 # Rock, Paper, Scissors
+
+puts "Your move: rock, paper, or scissors?"
+user_move = gets.chomp
+
+moves = ["rock", "paper", "scissors"]
+
+computer_move = moves.sample
+puts computer_move
+
+if user_move == computer_move
+  puts "It's a tie!"
+  elsif user_move == "rock" && computer_move == "scissors"
+    puts "You win!"
+  elsif user_move == "rock" && computer_move == "paper"
+    puts "You lose!"
+  elsif user_move == "paper" && computer_move == "rock"
+    puts "You win!"
+  elsif user_move == "paper" && computer_move == "scissors"
+    puts "You lose!"
+  elsif user_move == "scissors" && computer_move == "rock"
+    puts "You lose!"
+  elsif user_move == "scissors" && computer_move == "paper"
+    puts "You win!"
+end
