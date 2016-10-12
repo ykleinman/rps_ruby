cebaf1a5863271229686f82b46b084b29407e186
Yaniv Kleinman <yaniv.kleinman@gmail.com>
2016-10-11 20:31:35 -0500

    game_challenge3

--- rps.rb
+++ rps.rb
@@ -2,25 +2,34 @@
 
 puts "Your move: rock, paper, or scissors?"
 user_move = gets.chomp.downcase
+
 moves = ["rock", "paper", "scissors"]
 
 computer_move = moves.sample
-puts computer_move
 
-if user_move == computer_move
-  puts "It's a tie!"
-  elsif user_move == "rock" && computer_move == "scissors"
-    puts "You win!"
-  elsif user_move == "rock" && computer_move == "paper"
-    puts "You lose!"
-  elsif user_move == "paper" && computer_move == "rock"
-    puts "You win!"
-  elsif user_move == "paper" && computer_move == "scissors"
-    puts "You lose!"
-  elsif user_move == "scissors" && computer_move == "rock"
-    puts "You lose!"
-  elsif user_move == "scissors" && computer_move == "paper"
-    puts "You win!"
-  elsif user_move != moves
-    puts "That doesn't seem to be a valid move."
+
+while user_move != "quit"
+  puts "YOU CHOSE: #{user_move}"
+  puts "THE COMPUTER CHOSE: #{computer_move}"
+  if user_move == computer_move
+    puts "Who wins? It's a tie!"
+    elsif user_move == "rock" && computer_move == "scissors"
+      puts "You win!"
+    elsif user_move == "rock" && computer_move == "paper"
+      puts "You lose!"
+    elsif user_move == "paper" && computer_move == "rock"
+      puts "You win!"
+    elsif user_move == "paper" && computer_move == "scissors"
+      puts "You lose!"
+    elsif user_move == "scissors" && computer_move == "rock"
+      puts "You lose!"
+    elsif user_move == "scissors" && computer_move == "paper"
+      puts "You win!"
+    elsif user_move != moves
+      puts "That doesn't seem to be a valid move."
+  end
+  puts "Your move: rock, paper, or scissors?"
+  user_move = gets.chomp.downcase
+  
 end
+puts 'Come again soon!'
